e89a7231d2b198b2aec1a2d3c0d333258bb70d45
Nhalim Yuri <nhalim.uni@gmail.com>
2021-04-20 22:06:07 -0500

    Update dataConnection.py

--- tierdatalake/dataConnection.py
+++ tierdatalake/dataConnection.py
@@ -4,7 +4,7 @@ from sqlalchemy import types, create_engine
 import time
 
 cx_Oracle.init_oracle_client(lib_dir = r"C:\Program Files\Python36\instantclient_19_6")
-engine = create_engine('oracle://USR_UNIVERSAL:r4t10svp3r20@10.5.112.35:1521/dgpp', echo=False)
+engine = create_engine('oracle://USUARIO:PASSWORD@IP:1521/dgpp', echo=False)
 
 def qDataLakeConn(query):
     db_tables = pd.read_sql_query(query,engine)
